d65a00599df00294c50a41f7aa6a8eb269d1faae
Adão Lima <adaoolima@gmail.com>
2019-11-19 12:38:47 -0300

    Comment how is being applied by tutorial the ForestAdmin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
+// --------- Old way to use ForestAdmin
+  // const ForestAdmin = require('forest-express-sequelize');
+  // const { sequelize } = require('./models');
+  // const secret = require('../config/secret');
+//----------
+
 const path = require('path');
 const express = require('express');
-
 const app = express();
+
+//--------- Old way to use ForestAdmin
+  // app.use(
+  //   ForestAdmin.init({
+  //     modelsDir: path.resolve('./src/models'),
+  //     envScret: secret.FOREST_ENV_SECRET,
+  //     authSecret: secret.FOREST_AUTH_SECRET,
+  //     sequelize
+  //   })
+  // );
+//-----------
 
 //static files
 app.use(express.static('public'));
