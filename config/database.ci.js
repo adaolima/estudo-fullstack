c4252ad5ee15d43ce89cd08f542d051122464f74
Adão Lima <adaoolima@gmail.com>
2019-11-20 00:25:10 -0300

    Fixed ci database config

--- config/database.ci.js
+++ config/database.ci.js
@@ -1,10 +1,6 @@
-  
-const secret = require('./secret');
-
-module.exports = {
+  module.exports = {
   test: {
     username: 'postgres',
-    password: secret.DATABASE_PASSWORD,
     database: 'mymoney_ci_test',
     host: '127.0.0.1',
     dialect: 'postgres',
